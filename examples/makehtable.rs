2bc50ebe20f8e5b16c03ad0880140d7d3c72d8c7
Oliver Funk <oliver.funk@thoughtexpress.com>
2018-12-13 19:03:38 +0200

    Added row and coloumn iterators

--- examples/makehtable.rs
+++ examples/makehtable.rs
@@ -3,23 +3,23 @@ extern crate htable;
 use htable::*;
 
 fn main() {
-    let mut ht = HTable::new();
-    let mut test = HTableRow::new(vec![
-        HTableTypes::Integer(10),
-        HTableTypes::Integer(11),
-        HTableTypes::Field("oli".to_string()),
-    ]);
-    test.set_row_data(vec![
-        HTableTypes::Integer(10),
-        HTableTypes::Integer(11),
-        HTableTypes::Field("oliVar".to_string()),
-    ]);
-    let test2 = HTableRow::new(vec![
-        HTableTypes::Integer(10),
-        HTableTypes::Integer(11),
-        HTableTypes::Field("is the best".to_string()),
-    ]);
-    ht.append_row(test);
-    ht.append_row(test2);
-    println!("{:?}", ht);
+//    let mut ht = HTable::new();
+//    let mut test = HTableRow::new(vec![
+//        HTableItems::IntegerItem(10),
+//        HTableItems::IntegerItem(11),
+//        HTableItems::FieldItem("oli".to_string()),
+//    ]);
+//    test.set_row_data(vec![
+//        HTableItems::IntegerItem(10),
+//        HTableItems::IntegerItem(11),
+//        HTableItems::FieldItem("oliVar".to_string()),
+//    ]);
+//    let test2 = HTableRow::new(vec![
+//        HTableItems::IntegerItem(10),
+//        HTableItems::IntegerItem(11),
+//        HTableItems::FieldItem("is the best".to_string()),
+//    ]);
+//    ht.append_row(test);
+//    ht.append_row(test2);
+//    println!("{:?}", ht);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,84 +1,108 @@
 use std::mem;
 
 #[derive(Debug, Clone)]
-pub enum HTableTypes {
-    Integer(i64),
-    Float(f64),
-    Field(String),
-    Boolean(bool),
+pub enum HTableItems {
+    IntegerItem(i64),
+    FloatItem(f64),
+    FieldItem(String),
+    BooleanItem(bool),
 }
 
 #[derive(Debug)]
-pub struct HTableRow(Vec<HTableTypes>);
+pub struct IterHTableData<'a> {
+    curr_idx: usize,
+    items: Vec<&'a HTableItems>,
+}
 
-impl HTableRow {
-    pub fn new(row_data: Vec<HTableTypes>) -> Self {
-        HTableRow(row_data)
+impl<'a> IterHTableData<'a> {
+    pub fn new(row_data: Vec<&'a HTableItems>) -> Self {
+        IterHTableData {
+            curr_idx: 0,
+            items: row_data
+        }
     }
+}
 
-    pub fn reset_row_data(&mut self, row_data: Vec<HTableTypes>) {
-        self.0 = row_data;
-    }
+impl<'a> Iterator for IterHTableData<'a> {
+    type Item = &'a HTableItems;
 
-    pub fn set_row_data(&mut self, row_data: Vec<HTableTypes>) {
-        if self.0.len() != row_data.len() {
-            panic!("The number of columns in the row you are trying to add does not match the number of columns in the table")
+    fn next(&mut self) -> Option<<Self as Iterator>::Item> {
+        if self.curr_idx >= self.items.len(){
+            None
         } else {
-            for col_idx in 0..row_data.len() {
-                let matched = {
-                    let row_result = &row_data[col_idx];
-                    let self_row_result = &self.0[col_idx];
-
-                    match (row_result, self_row_result) {
-                        (HTableTypes::Integer(_), HTableTypes::Integer(_))
-                        | (HTableTypes::Float(_), HTableTypes::Float(_))
-                        | (HTableTypes::Field(_), HTableTypes::Field(_))
-                        | (HTableTypes::Boolean(_), HTableTypes::Boolean(_)) => true,
-                        _ => false,
-                    }
-                };
-
-                if matched {
-                    mem::replace(&mut self.0[col_idx], row_data[col_idx].clone());
-                } else {
-                    panic!("Row types do not match.")
-                }
-            }
+            self.curr_idx += 1;
+            Some(self.items[self.curr_idx - 1])
         }
     }
 }
 
 #[derive(Debug)]
-pub struct HTable(Vec<Vec<HTableTypes>>);
+pub struct HTable(Vec<Vec<HTableItems>>);
 
 impl HTable {
     pub fn new() -> Self {
         HTable(Vec::new())
     }
 
-    pub fn append_row(&mut self, row: HTableRow) {
+    pub fn append_row(&mut self, row: Vec<HTableItems>) {
         if self.0.len() == 0 {
             // table is uninitialised
-            for row_data in row.0 {
+            for row_data in row {
                 self.0.push(vec![row_data]);
             }
-        } else if self.0.len() != row.0.len() {
+        } else if self.0.len() != row.len() {
             panic!("The number of columns in the row you are trying to add does not match the number of columns in the table")
         } else {
-            for col_idx in 0..row.0.len() {
-                let row_result = &row.0[col_idx];
+            for col_idx in 0..row.len() {
+                let row_result = &row[col_idx];
                 let table_col = &mut self.0[col_idx];
 
                 match (row_result, table_col.as_slice()) {
-                    (HTableTypes::Integer(_), [HTableTypes::Integer(_)])
-                    | (HTableTypes::Float(_), [HTableTypes::Float(_)])
-                    | (HTableTypes::Field(_), [HTableTypes::Field(_)])
-                    | (HTableTypes::Boolean(_), [HTableTypes::Boolean(_)]) => {
+                    (HTableItems::IntegerItem(_), [HTableItems::IntegerItem(_)])
+                    | (HTableItems::FloatItem(_), [HTableItems::FloatItem(_)])
+                    | (HTableItems::FieldItem(_), [HTableItems::FieldItem(_)])
+                    | (HTableItems::BooleanItem(_), [HTableItems::BooleanItem(_)]) => {
                         table_col.push(row_result.clone());
                     }
                     _ => panic!("Mismatched types between row and table"),
                 }
             }
+        }
+    }
+
+    pub fn number_of_columns(&self) -> usize {
+        self.0.len()
+    }
+
+    pub fn number_of_rows(&self) -> usize {
+        self.0[0].len()
+    }
+
+    pub fn iter_row(&self, row: usize) -> Option<impl Iterator<Item=&HTableItems>> {
+        let mut row_data = Vec::new();
+
+        if row >= 0 && row < self.number_of_rows() {
+            for c_inx in 0..self.number_of_columns() {
+                row_data.push(&self.0[c_inx][row]);
+            }
+
+            Some(IterHTableData::new(row_data))
+        }else{
+            None
+        }
+    }
+
+    pub fn iter_col(&self, col: usize) -> Option<impl Iterator<Item=&HTableItems>> {
+        let mut col_data = Vec::new();
+
+        if col >= 0 && col < self.number_of_columns() {
+            for r_idx in 0..self.number_of_columns() {
+                col_data.push(&self.0[col][r_idx]);
+            }
+
+            Some(IterHTableData::new(col_data))
+        }else{
+            None
         }
     }
 }
